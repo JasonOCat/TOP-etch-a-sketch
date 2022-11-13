79b7039a896b9891df240d8eca47e49a3de86e7e
JasonOCat <jason.catbagan@gmail.com>
2022-11-13 14:59:08 +0100

    Do sketch

---
+++ js/javascript.js
@@ -0,0 +1,46 @@
+
+
+const containerDiv = document.querySelector('.container');
+
+function changeBackground(event) {
+    event.target.style.backgroundColor = "green";
+}
+
+
+
+function askGridSize() {
+    let gridSize;
+    let isVadidSize = false;
+    do {
+        gridSize = prompt("Choose a grid size between 1 and 100");
+        isVadidSize = !isNaN(gridSize) && gridSize >= 1 && gridSize <= 100;
+        if (!isVadidSize) {
+            alert("Wrong size format");
+        }
+    }
+    while (!isVadidSize);
+
+    containerDiv.textContent = "";
+
+    buildGrid(gridSize);
+    const squares = document.querySelectorAll('.square');
+    squares.forEach(square => square.addEventListener('mouseover', changeBackground));
+}
+
+function buildGrid(gridSize) {
+    let squareDiv;
+    for (let i = 0; i < gridSize * gridSize; i++) {
+        squareDiv = document.createElement('div');
+        squareDiv.setAttribute('class', 'square');
+        squareDiv.style.width = `calc(100% / ${gridSize})`;
+        containerDiv.appendChild(squareDiv);
+    }
+
+}
+
+buildGrid(4);
+
+
+const squares = document.querySelectorAll('.square');
+squares.forEach(square => square.addEventListener('mouseover', changeBackground));
+
